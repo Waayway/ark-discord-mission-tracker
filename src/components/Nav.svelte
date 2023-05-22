4c2dbe437139a0e02423ae2ab0d5f63344892b6c
Waayway <Thijs@vanwaaij.com>
2023-05-22 22:42:05 +0200

    another fix same stuff but on the nav tried in the browser and changed the wrong stuff....

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,16 +1,22 @@
-<script lang="ts">  
-import MdArrowBack from 'svelte-icons/md/MdArrowBack.svelte'
-export let userData: {
-        name?: string | null | undefined;
-        email?: string | null | undefined;
-        image?: string | null | undefined;
-    } | undefined;
+<script lang="ts">
+	import MdArrowBack from 'svelte-icons/md/MdArrowBack.svelte';
+	export let userData:
+		| {
+				name?: string | null | undefined;
+				email?: string | null | undefined;
+				image?: string | null | undefined;
+		  }
+		| undefined;
 </script>
 
-<nav class="w-screen bg-sapphire flex justify-between py-8 px-4">
-    <div class="text-2xl flex items-center"><a href="/authorized" class="flex h-12 whitespace-nowrap items-center"><MdArrowBack /> Back to dashboard</a></div>
-    <div class="flex items-center gap-4">
-        <h3 class="text-2xl">{userData?.name}</h3>
-        <img src={userData?.image} alt="" class="rounded-full bg-green h-24">
-    </div>
+<nav class="w-full bg-sapphire flex justify-between py-8 px-4">
+	<div class="text-2xl flex items-center">
+		<a href="/authorized" class="flex h-12 whitespace-nowrap items-center"
+			><MdArrowBack /> Back to dashboard</a
+		>
+	</div>
+	<div class="flex items-center gap-4">
+		<h3 class="text-2xl">{userData?.name}</h3>
+		<img src={userData?.image} alt="" class="rounded-full bg-green h-24" />
+	</div>
 </nav>
