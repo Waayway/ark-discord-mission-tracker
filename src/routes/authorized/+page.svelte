272a0282366f17367086516c4c6c4d0874d71b18
Waayway <Thijs@vanwaaij.com>
2023-12-21 21:05:25 +0100

    Add art per map

--- src/routes/authorized/+page.svelte
+++ src/routes/authorized/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { back_url } from 'lib/stores';
 	import type { PageData } from './$types';
+	import Gen1Art from 'assets/Gen_01_Art.jpg';
+	import Gen2Art from 'assets/Gen_02_Art.jpg';
 
 	export let data: PageData;
 	back_url.set('');
@@ -8,8 +10,9 @@
 
 <main class="grid grid-cols-2 place-items-center text-text p-4 gap-4 md:mx-32 lg:mx-48 xl:mx-64 2xl:mx-96">
 	{#each data.page_data ?? [] as map}
-		<a href="/authorized/{map.name}/" class="bg-surface2 text-4xl p-8 w-full h-full text-center rounded-2xl">
-			{map.name}
+		<a href="/authorized/{map.name}/" class="bg-surface2 text-4xl p-8 w-full h-full text-center rounded-2xl flex flex-col gap-4">
+			<span>{map.name}</span>
+			<img src={map.name.includes('1') ? Gen1Art : Gen2Art} alt="Art" class="rounded-xl">
 		</a>
 	{/each}
 </main>
